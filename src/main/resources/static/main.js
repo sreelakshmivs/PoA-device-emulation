2db93058ed83cdb8564247e02043ca17f185294d
Nicklas Nyström <nicklas.nystrom@xarepo.com>
2022-04-22 12:41:28 +0200

    Add icons and progress bar

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,17 +1,24 @@
+const contents = document.getElementById("contents");
+const progressBarContainer = document.getElementById("progress-bar-container");
 const fetchPoaButton = document.getElementById("fetch-poa");
 const certificateButton = document.getElementById("fetch-certificate");
 const provideLocationButton = document.getElementById("provide-location");
+const displayJwtButton = document.getElementById("display-jwt");
+const displayJsonButton = document.getElementById("display-json");
 const tokenDisplay = document.getElementById("token");
-const jwtDisplay = document.getElementById("jwt");
+const tokenTextArea = document.getElementById("token-text");
 const providingLocationMessage = document.getElementById("providing-location");
+let token = "";
 
 fetchPoaButton.onclick = () => {
   fetchPoaButton.disabled = true;
   fetch("/device/fetch-poa")
+    .then(() => runProgressBar())
     .then(() => fetch("/device/poa"))
     .then((response) => response.text())
-    .then((token) => {
-      certificateFetchingStage(token);
+    .then((aToken) => {
+      token = aToken;
+      certificateFetchingStage();
     })
     .catch(handleError);
 };
@@ -19,17 +26,22 @@ fetchPoaButton.onclick = () => {
 certificateButton.onclick = () => {
   certificateButton.disabled = true;
   fetch("/device/fetch-certificate")
-  .then(readyToProvideLocationStage)
-  .catch(handleError);
+    .then(() => runProgressBar())
+    .then(readyToProvideLocationStage)
+    .catch(handleError);
 };
 
 provideLocationButton.onclick = () => {
-    certificateButton.disabled = true;
-    fetch("/device/provide-location")
+  certificateButton.disabled = true;
+  fetch("/device/provide-location")
+    .then(() => runProgressBar(3000))
     .then(providingLocationStage)
     .catch(handleError);
-  };
+};
 
+displayJwtButton.onclick = displayJwt;
+
+displayJsonButton.onclick = displayJson;
 
 function parseJwt(token) {
   try {
@@ -39,8 +51,9 @@ function parseJwt(token) {
   }
 }
 
-function certificateFetchingStage(token) {
-  displayToken(token);
+function certificateFetchingStage() {
+  displayJwt(token);
+  show(tokenDisplay);
   hide(fetchPoaButton);
   show(certificateButton);
 }
@@ -51,27 +64,45 @@ function readyToProvideLocationStage() {
   show(provideLocationButton);
 }
 
-function displayToken(token) {
-  const parsedToken = parseJwt(token);
-  const jsonString = JSON.stringify(parsedToken, null, 4);
-  jwtDisplay.innerHTML = jsonString;
+function displayToken() {
   show(tokenDisplay);
 }
 
+function displayJwt() {
+  tokenTextArea.innerHTML = token;
+}
+
+function displayJson() {
+  const parsedToken = parseJwt(token);
+  const jsonString = JSON.stringify(parsedToken, null, 4);
+  tokenTextArea.innerHTML = jsonString;
+}
+
 function providingLocationStage() {
-    hide(provideLocationButton);
-    show(providingLocationMessage);
+  hide(provideLocationButton);
+  show(providingLocationMessage);
 }
 
 function show(element) {
-    element.classList.remove("d-none");
+  element.classList.remove("d-none");
 }
 
 function hide(element) {
-    element.classList.add("d-none");
+  element.classList.add("d-none");
 }
 
 function handleError(e) {
   console.log(e);
   alert("Something went wrong");
+}
+
+function runProgressBar(time = 1500) {
+  const progressBar = $(".progress-bar");
+  progressBar.css("width", "0%");
+  hide(contents);
+  show(progressBarContainer);
+  return progressBar.animate({ width: "100%" }, time, () => {
+    hide(progressBarContainer);
+    show(contents);
+  });
 }
